f2c7f76462e6cb4c26e8212f25c1706b06441e26
CAMILO ALEJANDRO ALFONSO SANCHEZ <ca.alfonso967@isc331.uniandes.edu.co>
2017-10-18 16:02:25 -0500

    Initial commit

--- PermanentDataStorage/ViewController.swift
+++ PermanentDataStorage/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    
+    @IBAction func Submit(_ sender: Any) {
+        
+        let input = textField.text
+        
+        UserDefaults.standard.set(input, forKey: "number")
+        
+        let numberObject = UserDefaults.standard.object(forKey: "number")
+        
+        if let number = numberObject as? String {
+            print("Success")
+            print(number)
+        }
+        
+    }
+    
+    @IBAction func Retrieve(_ sender: Any) {
+        
+        let numberObject = UserDefaults.standard.object(forKey: "number")
+        
+        if let number = numberObject as? String {
+            result.text = number
+        }
 
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
+        
+        }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
